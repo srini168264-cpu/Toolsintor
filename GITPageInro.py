0fcf3f54732570f4de86fdc536aaab2c07d55d65
srini168264-cpu <129779626+srini168264-cpu@users.noreply.github.com>
2024-03-05 16:45:19 -0500

    Add files via upload

---
+++ GITPageInro.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Ecosystem
+
+# In[ ]:
+
+
+
+
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ### Objectives:
+# 
+# - **List popular languages for Data Science**
+# - **Introduce essential Data Science Libraries**
+# - **Explore basic Arithmetic Expressions in Python**
+# - **Understand the role of Data Science Tools like Jupyter Notebooks**
+# - **Highlight key takeaways from the course**
+
+# Data Science Languages in Jupyter Notebooks
+# 
+# 1 .Python: Primary language for Jupyter Notebooks, with extensive support for data analysis libraries like NumPy, Pandas, Matplotlib, and scikit-learn.
+# 
+# 2. R: Widely used for statistical analysis and visualization within Jupyter Notebooks, especially in academia and research.
+# 
+# 
+# 3 Julia: Known for its high-performance computing capabilities, Julia is gaining popularity in data science, especially for tasks involving large datasets.
+# 
+# 
+
+# Data Science Libraries for Jupyter Notebooks
+# 
+# 1 . NumPy:** Fundamental package for scientific computing in Python, providing support for large, multi-dimensional arrays and matrices
+# 
+# 2. **Pandas:** Data manipulation and analysis library offering data structures like DataFrame, making it easy to work with structured data.
+# 
+# 
+# 3. **Matplotlib:** Comprehensive 2D plotting library for creating static, interactive, and animated visualizations in Jupyter Notebooks.
+
+# ### Data Science Tools
+# 
+# 1. **Jupyter Notebooks:** Interactive and versatile tool for creating and sharing documents containing live code, equations, visualizations, and narrative text.
+# 
+# 2. **Anaconda:** Distribution of Python and R programming languages for data science, including a package manager and a variety of pre-installed libraries.
+# 
+# 3. **Tableau:** Powerful data visualization tool that allows users to create inte
+
+# ### Arithmetic Expressions Examples
+#  
+#  **Addition:**
+#  
+#  **Subtraction:**
+#  
+#  **Multiplication**
+
+# In[7]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[8]:
+
+
+#This will convert 200 minutes to hours by diving by 60.
+hours = 200/60
+print(hours)
+
+
+# ## Author
+# 
+# Srinivas Reddy
+
+# In[ ]:
+
+
+
+
